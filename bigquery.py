819004ee05fb7da760e333ded3f09b9204ba5686
unknown <antoine.cosne@ik.me>
2026-05-20 10:45:30 +0200

    Made Google credentials more reliable by removing temp file logic.

--- bigquery.py
+++ bigquery.py
@@ -4,9 +4,10 @@ from google.oauth2 import service_account
 import pandas as pd
 from config import Config
 
-def get_google_credentials(credentials_path=None):
-    credentials_path = credentials_path or Config.GOOGLE_APPLICATION_CREDENTIALS
-    return service_account.Credentials.from_service_account_file(credentials_path)
+def get_google_credentials():
+    return service_account.Credentials.from_service_account_info(
+        Config.credentials_info  # Utilise le dictionnaire directement
+    )
 
 credentials = get_google_credentials()
 client = bigquery.Client(project=Config.BQ_PROJECT, credentials=credentials)
